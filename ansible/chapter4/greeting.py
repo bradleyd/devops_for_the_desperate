69d3a1a788a7537c8f09f156e121464e0c67116f
Bradley Smith <bradleydsmith@gmail.com>
2020-04-12 10:52:11 -0600

    add chapter 4. add chpater 5. misc fixes

--- ansible/chapter4/greeting.py
+++ ansible/chapter4/greeting.py
@@ -7,9 +7,5 @@ def hello():
     return "<h1 style='color:green'>Greetings!</h1>"
 
 
-@app.route('/<name>')
-def hello_name(name):
-    return "<h1 style='color:green'>Greetings {}!</h1>".format(name)
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True)
